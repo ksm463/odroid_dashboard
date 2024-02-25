9d32f6c9d78e3965c23d9b62a5454e58cb266d0a
ksm463 <113885176+ksm463@users.noreply.github.com>
2024-02-25 22:45:23 +0900

    del_telegram_token

--- src/utils/telegram.py
+++ src/utils/telegram.py
@@ -1,8 +1,8 @@
 import requests
 
 def send_telegram(message):
-    token = '6760963910:AAGp7hAMztb_nESlDSNPhQlUX1fajqKCCqE'
-    chat_id = '43517934'
+    token = 'bot_token'
+    chat_id = 'chat_id'
     send_text = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
     
     response = requests.get(send_text)
