2e979ff596cc551732299c8889ad2e589f8d6da9
ksm463 <113885176+ksm463@users.noreply.github.com>
2024-06-29 18:48:38 +0900

    Update telegram.py

--- src/utils/telegram.py
+++ src/utils/telegram.py
@@ -1,6 +1,16 @@
 import requests
 
-def send_telegram(message):
+def send_telegram(sensor_state, sensor_data):
+    message = None
+    if sensor_state == "very_high_temp":
+        message = f"경고: 온도 {sensor_data.temperature}°C가 매우 높습니다."
+    elif sensor_state == "high_temp":
+        message = f"경고: 온도 {sensor_data.temperature}°C가 비정상 범위에 진입했습니다."
+    elif sensor_state == "low_humidity":
+        message = f"주의: 습도 {sensor_data.humidity}%가 낮습니다."
+    elif sensor_state == "high_humidity":
+        message = f"주의: 습도 {sensor_data.humidity}%가 높습니다."
+    
     token = 'token_id'
     chat_id = 'chat_id'
     send_text = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
